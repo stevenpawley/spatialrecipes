b052087378b2ac68a07a5f7ec44fe2b6706b94b7
Steven Pawley <dr.stevenpawley@gmail.com>
2021-03-23 14:36:54 -0600

    u

--- R/step_knn_classdist.R
+++ R/step_knn_classdist.R
@@ -45,15 +45,16 @@ return_knn_classdists <- function(formula, x, y, k) {
 
 #' Spatial class distances step
 #'
-#' `step_knn_classdist` creates a *specification* of a recipe step that will add new
-#' features to a dataset based on the distances to each neighbor, per class.
+#' `step_knn_classdist` creates a *specification* of a recipe step that will add
+#' new features to a dataset based on the distances to each neighbor, per class.
 #'
-#' For example, if a dataset's outcome variable contains two classes, c("sand", "clay"),
-#' this step will append a new feature matrix with (n_samples, n_classes * neighbors) to
-#' the dataset. For the previous example with neighbors = 3, the step would create new
-#' features called "sand_neighbor1, "sand_neighbor2, "sand_neighbor3", "clay_neighbor1",
-#' "clay_neighbor2", "clay_neighbor3" with each feature representing the distance
-#' (in the feature space) to the closest k points of each class.
+#' For example, if a dataset's outcome variable contains two classes, c("sand",
+#' "clay"), this step will append a new feature matrix with (n_samples,
+#' n_classes * neighbors) to the dataset. For the previous example with
+#' neighbors = 3, the step would create new features called "sand_neighbor1,
+#' "sand_neighbor2, "sand_neighbor3", "clay_neighbor1", "clay_neighbor2",
+#' "clay_neighbor3" with each feature representing the distance (in the feature
+#' space) to the closest k points of each class.
 #'
 #' This step is based on the concept described in the 'fastknn' package
 #' (https://github.com/davpinto/fastknn) to perform a non-linear mapping of the
@@ -68,22 +69,22 @@ return_knn_classdists <- function(formula, x, y, k) {
 #'   surrounding observations.
 #' @param neighbors The number of closest neighbors to use in the distance
 #'   weighting. Default is 3.
-#' @param scale A boolean to indicate whether the selected columns should be standardized
-#'   before transformation. Default is `TRUE`.
-#' @param role role or model term created by this step, what analysis
-#'  role should be assigned?. By default, the function assumes
-#'  that resulting distance will be used as a predictor in a model.
+#' @param scale A boolean to indicate whether the selected columns should be
+#'   standardized before transformation. Default is `TRUE`.
+#' @param role role or model term created by this step, what analysis role
+#'   should be assigned?. By default, the function assumes that resulting
+#'   distance will be used as a predictor in a model.
 #' @param trained A logical that will be updated once the step has been trained.
 #' @param data Used internally to store the training data.
-#' @param columns A character string that contains the names of columns used in the
-#' transformation. This is `NULL` until computed by `prep.recipe()`.
-#' @param means A named numeric vector of means. This is `NULL` until computed by
-#' `prep.recipe()`.
-#' @param sds A named numeric vector of standard deviations. This is `NULL` until
-#'   computed by `prep.recipe()`.
+#' @param columns A character string that contains the names of columns used in
+#'   the transformation. This is `NULL` until computed by `prep.recipe()`.
+#' @param means A named numeric vector of means. This is `NULL` until computed
+#'   by `prep.recipe()`.
+#' @param sds A named numeric vector of standard deviations. This is `NULL`
+#'   until computed by `prep.recipe()`.
 #' @param skip A logical to skip training.
 #' @param id An identifier for the step. If omitted then this is generated
-#' automatically.
+#'   automatically.
 #'
 #' @return An updated version of `recipe` with the new step added to the
 #'   sequence of existing steps (if any).
@@ -102,9 +103,6 @@ step_knn_classdist <- function(
   skip = FALSE,
   id = recipes::rand_id("knn_classdist")) {
 
-  if (!"nabor" %in% installed.packages()[, 1])
-    stop("step_knn_classdist requires the package `nabor` to be installed")
-
   recipes::recipes_pkg_check("nabor")
   terms <- recipes::ellipse_check(...)
 
@@ -115,7 +113,7 @@ step_knn_classdist <- function(
     recipe,
     step_knn_classdist_new(
       terms = terms,
-      class = rlang::enquos(class),
+      class = class,
       trained = trained,
       role = role,
       neighbors = neighbors,
@@ -156,7 +154,7 @@ prep.step_knn_classdist <- function(x, training, info = NULL, ...) {
 
   # First translate the terms argument into column name
   col_names <- terms_select(terms = x$terms, info = info)
-  class_name <- terms_select(x$class, info = info)
+  class_name <- x$class
 
   # Check selections
   if (!inherits(training[[class_name]], "factor"))
---
+++ tests/testthat/test-step_knn_classdist.R
@@ -0,0 +1,22 @@
+library(modeldata)
+data("Sacramento")
+
+test_that("test step_knn_classdist basic", {
+
+  rec_obj <- Sacramento %>%
+    recipe(price ~ .) %>%
+    step_knn_classdist(latitude, longitude, class = "type", neighbors = 1)
+
+  prepped <- prep(rec_obj)
+  result <- juice(prepped)
+
+  names(result)
+
+  expect_true(all(
+    c(
+      "Condo_neighbor1",
+      "Multi_Family_neighbor1",
+      "Residential_neighbor1"
+    ) %in% names(result)
+  ))
+})
