f2eac43a8d47b6397472c61ee15c2151e7978068
Steven Pawley <dr.stevenpawley@gmail.com>
2022-12-15 23:26:34 -0700

    Extra notes added to kernel function

--- R/kernels.R
+++ R/kernels.R
@@ -10,6 +10,19 @@ gaussian_kernel <- function(distances, k) {
 
   # create adaptive window width
   # get probability based on the number of neighbours
+  # idea is to set sigma equal to the inverse of twice the number of dimensions in the data
+  # for example 1 / (2 * 2) = 1/4 for two dimensions
+  # for three dimensions, 1 / (2 * 3) = 1/6
+  # the idea that the distance between points in high-dimensional space is
+  # typically smaller than the distance between points in low-dimensional space.
+  # Therefore, a smaller sigma value is needed to capture the similarity between
+  # points in high-dimensional space.
+
+  # for KNN using a large number of neighbors, 1 / 2k creates a small kernel
+  # bandwith that will be more sensitive to points that are close to the prediction
+  # point. Conversely, KNN with a small number of neighbours, 1 / 2k makes a
+  # large bandwidth that will smooth out the prediction and will reduce the
+  # variance of the KNN model
   p <- 1 / (2 * k)
 
   # get percentile for probability - how many standard deviations from the mean
